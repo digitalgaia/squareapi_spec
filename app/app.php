953cebf17e26e4c975e0d465e0a6a7baf9e90246
Ahmad Rahimie <newrehmi@gmail.com>
2016-06-28 06:46:33 +0800

    composer and routing update

--- app/app.php
+++ app/app.php
@@ -28,107 +28,87 @@ $app->map->middleware(function($exe)
 	}
 });
 
-$app->map->addRoutes(array(
-	'rule' => array(
-		'path' => '/rule-of-thumbs',
-		'execute' => function($exe)
+$app->map['rule']->get('/rule-of-thumbs')->execute(function($exe)
+{
+	return $exe->layout->set('view', $exe->view->create('rule'))->render();
+});
+
+$app->map['error']->requestable(false)->execute(function($exe)
+{
+	return $exe->layout
+	->set('view', $exe->view->create('404')
+		->set('exception', $exe->param('exception')))
+	->render();
+});
+
+$app->map['model']->any('/model')
+	->middleware(function($exe)
+	{
+		$path = $exe->path['app']->path('model');
+
+		$dir = opendir($path);
+
+		$models = array();
+
+		while(($file = readdir($dir)) !== false)
 		{
-			return $exe->layout->set('view', $exe->view->create('rule'))->render();
+			if(strpos($file, '.json') === false)
+				continue;
+
+			$model = str_replace('.json', '', $file);
+			$models[$model] = $model;
 		}
-		),
-	'error' => array(
-		'path' => false,
-		'execute' => function($exe)
+
+		$exe->modelList = $models;
+
+		$response = $exe->next($exe);
+
+		if(!is_array($response))
+			return $response;
+
+		return $exe->layout->set('view', $exe->view->create('model', array(
+			'models' => $models,
+			'view' => $response['view'],
+			'data' => $response['data']
+			)))->render();
+	})->group(function($models)
+	{
+		$models['add']->any('/add')->execute('controller=model@add');
+
+		$models['view']->any('/[:model]')->execute('controller=model@view');
+
+		$models['delete']->any('/delete/[:model]')->execute(function($exe)
 		{
-			return $exe->layout
-			->set('view', $exe->view->create('404')
-				->set('exception', $exe->param('exception')))
-			->render();
-		}),
-	'model' => array(
-		'path' => '/model',
-		'middleware' => function($exe)
+			$path = realpath($exe->path['app']->file('model/'.$exe->param('model').'.json'));
+
+			unlink($path);
+
+			return $exe->redirect->flash('success', 'model ['.$exe->param('model').'] has been deleted.')->route('add');
+		});
+
+		$models['edit']->any('/edit/[:model]')->execute('controller=model@edit');
+	});
+
+$app->map['spec']->get('/[:path?]')->execute(function($exe)
+{
+	$api = json_decode($exe->path['app']->getContents('data/index.json'), true);
+
+	$path = $exe->param('path');
+
+	if($path)
+	{
+		if($exe->path['app']->isExists($file = 'data/'.$path.'.json'))
 		{
-			$path = $exe->path['app']->path('model');
+			$data = json_decode($exe->path['app']->getContents($file), true);
+		}
+	}
 
-			$dir = opendir($path);
-
-			$models = array();
-
-			while(($file = readdir($dir)) !== false)
-			{
-				if(strpos($file, '.json') === false)
-					continue;
-
-				$model = str_replace('.json', '', $file);
-				$models[$model] = $model;
-			}
-
-			$exe->modelList = $models;
-
-			$response = $exe->next($exe);
-
-			if(!is_array($response))
-				return $response;
-
-			return $exe->layout->set('view', $exe->view->create('model', array(
-				'models' => $models,
-				'view' => $response['view'],
-				'data' => $response['data']
-				)))->render();
-		},
-		'subroutes' => array(
-			'add' => array(
-				'path' => '/add',
-				'execute' => 'controller=model@add'
-				),
-			'view' => array(
-				'path' => '/[:model]',
-				'execute' => 'controller=model@view'
-				),
-			'delete'=> array(
-				'path' => '/delete/[:model]',
-				'execute' => function($exe)
-				{
-					$path = realpath($exe->path['app']->file('model/'.$exe->param('model').'.json'));
-
-					unlink($path);
-
-					return $exe->redirect->flash('success', 'model ['.$exe->param('model').'] has been deleted.')->route('add');
-				}),
-			'edit' => array(
-				'path' => '/edit/[:model]',
-				'execute' => 'controller=model@edit'
-				)
-			)
-		)
-	));
-
-$app->map->addRoutes(array(
-	'spec' => array(
-		'path' => '/[:path?]',
-		'execute' => function($exe)
-			{
-				$api = json_decode($exe->path['app']->getContent('data/index.json'), true);
-
-				$path = $exe->param('path');
-
-				if($path)
-				{
-					if($exe->path['app']->isExists($file = 'data/'.$path.'.json'))
-					{
-						$data = json_decode($exe->path['app']->getContents($file), true);
-					}
-				}
-
-				return $exe->layout
-				->set('view', $exe->view->create('api', array(
-					'apis' => $api,
-					'path' => $path,
-					'data' => $data
-					)))->render();
-			}
-		)
-	));
+	return $exe->layout
+	->set('view', $exe->view->create('api', array(
+		'apis' => $api,
+		'path' => $path,
+		'data' => $data
+		)))->render();
+});
 
 return $app;
